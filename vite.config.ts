8272f2e1c83292ebafca38480e972022e09982fa
ayushgpt01 <ayushgpt043@gmail.com>
2023-08-05 15:20:40 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./rock-paper-scissors",
+  base: "https://github.com/ayushgpt01/rock-paper-scissors",
   plugins: [react(), svgr()],
 });
