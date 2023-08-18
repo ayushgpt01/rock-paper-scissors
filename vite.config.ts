28d596a3466d87dee3450c0e5c411e90ea6de9a6
ayushgpt01 <ayushgpt043@gmail.com>
2023-08-18 15:13:30 +0530

    github actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,15 @@ import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "https://github.com/ayushgpt01/rock-paper-scissors",
-  plugins: [react(), svgr()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react(), svgr()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/rock-paper-scissors/";
+  }
+
+  return config;
 });
